07dddcb349dd96b9dca46845c439ea81c51749bd
leepfrog1414 <ashleejflanagan@hotmail.com>
2018-02-10 13:14:18 -0500

    Update import.js

--- import.js
+++ import.js
@@ -1,1 +1,7 @@
-test
+// ES6 Modules - recommended
+import implement, { Interface, type } from 'implement-js'
+
+// CommonJS
+const implementjs = require('implement-js')
+const implement = implementjs.default
+const { Interface, type } = implementjs

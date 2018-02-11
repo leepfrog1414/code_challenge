5e96f9bad83d5cc0f9205e3a34f2e0ae244c1a3b
leepfrog1414 <leepfrog1414@yahoo.com>
2018-02-11 16:48:28 -0500

    Clean project
    Removing initially committed files not longer in working set

--- import.js
+++
@@ -1,7 +0,0 @@
-// ES6 Modules - recommended
-import implement, { Interface, type } from 'implement-js'
-
-// CommonJS
-const implementjs = require('implement-js')
-const implement = implementjs.default
-const { Interface, type } = implementjs
